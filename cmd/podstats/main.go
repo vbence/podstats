a706794ca2470a17437efd041bdaa035608aa4e1
Bence <bence@Bences-MacBook-Pro.local>
2021-02-21 20:20:30 +0100

    Removed Watcher in favor of Lister; added notion of 'sources'.

--- cmd/podstats/main.go
+++ cmd/podstats/main.go
@@ -10,6 +10,7 @@ import (
 	"path/filepath"
 	"regexp"
 	"strconv"
+	"sync"
 	"time"
 
 	cache "github.com/victorspringer/http-cache"
@@ -18,9 +19,7 @@ import (
 	"gopkg.in/inf.v0"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/scheme"
 	rest "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
@@ -41,171 +40,123 @@ const (
 
 // Reading represents a single sensor reading, a value for a metric at a given time
 type Reading struct {
-	Key   string
 	Value float64
 	Time  string
 	Type  ReadingType
 }
 
-// Accept returns a metric updated with the new reading
-func (r *Reading) Accept(new *Reading) *Reading {
-	result := *r
-	if new.Type == Counter {
-		result.Value += new.Value
-		result.Time = new.Time
-	} else if new.Type == Instant {
-		result.Value = new.Value
-		result.Time = new.Time
-	}
-	return &result
-}
-
 // MetricsHolder represents a set of metrics in Prometheus's format
 type MetricsHolder struct {
-	lines   map[string]*Reading
-	channel chan interface{}
+	allLines map[string]map[string]*Reading
+	mutex    sync.RWMutex
 }
 
 // NewMetrics instantiates an empty MetricsHolder
 func NewMetrics() *MetricsHolder {
 	m := &MetricsHolder{
-		lines:   make(map[string]*Reading),
-		channel: make(chan interface{}),
+		allLines: make(map[string]map[string]*Reading),
+		mutex:    sync.RWMutex{},
 	}
-	go func() {
-		for {
-			w, ok := <-m.channel
-			reading := w.(*Reading)
-			if !ok {
-				break
-			}
-			if val, ok := m.lines[reading.Key]; ok {
-				m.lines[reading.Key] = val.Accept(reading)
-			} else {
-				m.lines[reading.Key] = reading
-			}
-		}
-	}()
 	return m
 }
 
-// Channel returns channel on which this MetricsHolder accepts new readings
-func (m *MetricsHolder) Channel() chan<- interface{} {
-	return m.channel
+// PutReadings upgrade reading for the given source (removing any existing readings for the source)
+func (m *MetricsHolder) PutReadings(source string, readings map[string]*Reading) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	m.allLines[source] = readings
 }
 
 // CreateHandler return a new `http.HandlerFunc` for a MetricsHolder
 func (m *MetricsHolder) CreateHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		for k, v := range m.lines {
-			w.Write([]byte(k))
-			w.Write([]byte(" "))
-			w.Write([]byte(fmt.Sprintf("%f", v.Value)))
-			w.Write([]byte("\n"))
-		}
-	}
-}
-
-// MetricsExtracor creates metrics out of Kubernetes objects
-type MetricsExtracor struct {
-	channel chan interface{}
-	holder  *MetricsHolder
-}
-
-// NewMetricsExtracor constructs a MetricsExtracor
-func NewMetricsExtracor(holder *MetricsHolder) *MetricsExtracor {
-	channel := make(chan interface{})
-	m := &MetricsExtracor{
-		channel: channel,
-		holder:  holder,
-	}
-	go func() {
-		done := false
-		for !done {
-			select {
-			case o := <-channel:
-				switch v := o.(type) {
-				case apiv1.Pod:
-					m.propagatePodSpecs(v)
-				case metricsv1beta1.PodMetrics:
-					m.propagatePodMetrics(v)
-				}
+		m.mutex.RLock()
+		defer m.mutex.RUnlock()
+		for _, lines := range m.allLines {
+			for k, v := range lines {
+				w.Write([]byte(k))
+				w.Write([]byte(" "))
+				w.Write([]byte(fmt.Sprintf("%f", v.Value)))
+				w.Write([]byte("\n"))
 			}
 		}
-	}()
-	return m
+	}
 }
 
-func (m *MetricsExtracor) propagatePodSpecs(pod apiv1.Pod) {
-	for _, con := range pod.Spec.Containers {
+func extractPodSpecs(pods []apiv1.Pod) map[string]*Reading {
+	r := make(map[string]*Reading)
+	for _, pod := range pods {
+
 		extras := make(map[string]string)
 		extras["pod-name"] = pod.Name
-		extras["container-name"] = con.Name
 
-		m.holder.Channel() <- &Reading{
-			Key:   "ps_memory_request_bytes" + renderLabels(extras),
-			Time:  strconv.FormatInt(pod.GetCreationTimestamp().Unix() * 1000, 10),
-			Type:  Instant,
-			Value: decToFloat64(con.Resources.Requests.Memory().AsDec()),
-		}
-		m.holder.Channel() <- &Reading{
-			Key:   "ps_memory_limit_bytes" + renderLabels(extras),
-			Time:  strconv.FormatInt(pod.GetCreationTimestamp().Unix() * 1000, 10),
-			Type:  Instant,
-			Value: decToFloat64(con.Resources.Limits.Memory().AsDec()),
-		}
-		m.holder.Channel() <- &Reading{
-			Key:   "ps_cpu_request_cores" + renderLabels(extras),
-			Time:  strconv.FormatInt(pod.GetCreationTimestamp().Unix() * 1000, 10),
-			Type:  Instant,
-			Value: decToFloat64(con.Resources.Requests.Cpu().AsDec()),
-		}
-		m.holder.Channel() <- &Reading{
-			Key:   "ps_cpu_limit_cores" + renderLabels(extras),
-			Time:  strconv.FormatInt(pod.GetCreationTimestamp().Unix() * 1000, 10),
-			Type:  Instant,
-			Value: decToFloat64(con.Resources.Limits.Cpu().AsDec()),
-		}
-		m.holder.Channel() <- &Reading{
-			Key:   "ps_storage_request_bytes" + renderLabels(extras),
-			Time:  strconv.FormatInt(pod.GetCreationTimestamp().Unix() * 1000, 10),
-			Type:  Instant,
-			Value: decToFloat64(con.Resources.Requests.StorageEphemeral().AsDec()),
-		}
-		m.holder.Channel() <- &Reading{
-			Key:   "ps_storage_limit_bytes" + renderLabels(extras),
-			Time:  strconv.FormatInt(pod.GetCreationTimestamp().Unix() * 1000, 10),
-			Type:  Instant,
-			Value: decToFloat64(con.Resources.Limits.StorageEphemeral().AsDec()),
+		for _, con := range pod.Spec.Containers {
+			extras["container-name"] = con.Name
+
+			r["ps_memory_request_bytes"+renderLabels(pod.Labels, extras)] = &Reading{
+				Time:  strconv.FormatInt(pod.GetCreationTimestamp().Unix()*1000, 10),
+				Type:  Instant,
+				Value: decToFloat64(con.Resources.Requests.Memory().AsDec()),
+			}
+			r["ps_memory_limit_bytes"+renderLabels(pod.Labels, extras)] = &Reading{
+				Time:  strconv.FormatInt(pod.GetCreationTimestamp().Unix()*1000, 10),
+				Type:  Instant,
+				Value: decToFloat64(con.Resources.Limits.Memory().AsDec()),
+			}
+			r["ps_cpu_request_cores"+renderLabels(pod.Labels, extras)] = &Reading{
+				Time:  strconv.FormatInt(pod.GetCreationTimestamp().Unix()*1000, 10),
+				Type:  Instant,
+				Value: decToFloat64(con.Resources.Requests.Cpu().AsDec()),
+			}
+			r["ps_cpu_limit_cores"+renderLabels(pod.Labels, extras)] = &Reading{
+				Time:  strconv.FormatInt(pod.GetCreationTimestamp().Unix()*1000, 10),
+				Type:  Instant,
+				Value: decToFloat64(con.Resources.Limits.Cpu().AsDec()),
+			}
+			r["ps_storage_request_bytes"+renderLabels(pod.Labels, extras)] = &Reading{
+				Time:  strconv.FormatInt(pod.GetCreationTimestamp().Unix()*1000, 10),
+				Type:  Instant,
+				Value: decToFloat64(con.Resources.Requests.StorageEphemeral().AsDec()),
+			}
+			r["ps_storage_limit_bytes"+renderLabels(pod.Labels, extras)] = &Reading{
+				Time:  strconv.FormatInt(pod.GetCreationTimestamp().Unix()*1000, 10),
+				Type:  Instant,
+				Value: decToFloat64(con.Resources.Limits.StorageEphemeral().AsDec()),
+			}
 		}
 	}
+
+	return r
 }
 
-func (m *MetricsExtracor) propagatePodMetrics(metrics metricsv1beta1.PodMetrics) {
-	for _, con := range metrics.Containers {
+func extractPodMetrics(allMetrics []metricsv1beta1.PodMetrics) map[string]*Reading {
+	r := make(map[string]*Reading)
+	for _, metrics := range allMetrics {
 		extras := make(map[string]string)
 		extras["pod-name"] = metrics.Name
-		extras["container-name"] = con.Name
 
-		m.holder.Channel() <- &Reading{
-			Key:   "ps_memory_usage_bytes" + renderLabels(metrics.Labels, extras),
-			Time:  strconv.FormatInt(metrics.Timestamp.Unix() * 1000, 10),
-			Type:  Instant,
-			Value: decToFloat64(con.Usage.Memory().AsDec()),
-		}
-		m.holder.Channel() <- &Reading{
-			Key:   "ps_cpu_usage_cores" + renderLabels(metrics.Labels, extras),
-			Time:  strconv.FormatInt(metrics.Timestamp.Unix() * 1000, 10),
-			Type:  Instant,
-			Value: decToFloat64(con.Usage.Cpu().AsDec()),
-		}
-		m.holder.Channel() <- &Reading{
-			Key:   "ps_storage_usage_bytes" + renderLabels(metrics.Labels, extras),
-			Time:  strconv.FormatInt(metrics.Timestamp.Unix() * 1000, 10),
-			Type:  Instant,
-			Value: decToFloat64(con.Usage.StorageEphemeral().AsDec()),
+		for _, con := range metrics.Containers {
+			extras["container-name"] = con.Name
+
+			r["ps_memory_usage_bytes"+renderLabels(metrics.Labels, extras)] = &Reading{
+				Time:  strconv.FormatInt(metrics.Timestamp.Unix()*1000, 10),
+				Type:  Instant,
+				Value: decToFloat64(con.Usage.Memory().AsDec()),
+			}
+			r["ps_cpu_usage_cores"+renderLabels(metrics.Labels, extras)] = &Reading{
+				Time:  strconv.FormatInt(metrics.Timestamp.Unix()*1000, 10),
+				Type:  Instant,
+				Value: decToFloat64(con.Usage.Cpu().AsDec()),
+			}
+			r["ps_storage_usage_bytes"+renderLabels(metrics.Labels, extras)] = &Reading{
+				Time:  strconv.FormatInt(metrics.Timestamp.Unix()*1000, 10),
+				Type:  Instant,
+				Value: decToFloat64(con.Usage.StorageEphemeral().AsDec()),
+			}
 		}
 	}
+	return r
 }
 
 func getOrDef(m map[string]float64, key string, def float64) float64 {
@@ -221,16 +172,11 @@ func decToFloat64(dec *inf.Dec) float64 {
 		math.Pow(10, float64(dec.Scale()))
 }
 
-// Channel returns channel on which this MetricsHolder accepts new readings
-func (m *MetricsExtracor) Channel() chan<- interface{} {
-	return m.channel
-}
-
 // Closer follows pod changes through the api
 type Closer func()
 
 // ShovelList reads data from a Lister
-func ShovelList(lister Lister, sink chan<- interface{}, log *zap.Logger) Closer {
+func ShovelList(lister Lister, name string, holder *MetricsHolder, log *zap.Logger) Closer {
 	close := make(chan struct{})
 	listOptions := metav1.ListOptions{AllowWatchBookmarks: true}
 	go func() {
@@ -243,73 +189,10 @@ func ShovelList(lister Lister, sink chan<- interface{}, log *zap.Logger) Closer 
 				if err != nil {
 					log.Error("Listing", zap.Error(err))
 				}
-				for _, v := range list {
-					//fmt.Printf("NewListItem: %+v\n", v)
-					sink <- v
-				}
+				holder.PutReadings(name, list)
 			case <-close:
 				done = true
 			}
-		}
-	}()
-	return func() {
-		close <- struct{}{}
-	}
-}
-
-// NewWatcher creates a Watcher object with the given endpoint
-func NewWatcher(watcher Watcher, sink chan<- interface{}, log *zap.Logger) Closer {
-	close := make(chan struct{})
-	//go doWatch(clientset, namespace, sink, close)
-	go func() {
-		done := false
-		bookmark := ""
-		for !done {
-			log.Info("Watcher connecting")
-			listOptions := metav1.ListOptions{Watch: true, AllowWatchBookmarks: true}
-			if bookmark != "" {
-				listOptions.ResourceVersion = bookmark
-			}
-			w, err := watcher.Watch(listOptions)
-			if err != nil {
-				log.Error("Watcher connecting", zap.Error(err))
-			} else {
-				events := w.ResultChan()
-				open := true
-			W:
-				for !done && open {
-					select {
-					case e, open := <-events:
-						if !open {
-							log.Info("Watcher disconnected")
-							w.Stop()
-							break W
-						}
-
-						if e.Type == watch.Bookmark {
-							pod := apiv1.Pod{}
-							err := scheme.Scheme.Convert(e.Object, &pod, nil)
-							if err != nil {
-								log.Error("Converting bookmark", zap.Error(err))
-							}
-							log.Debug("Bookmark received", zap.String("bookmark", pod.ResourceVersion))
-							bookmark = pod.ResourceVersion
-						} else {
-							// process
-							object, err := watcher.Convert(&e)
-							if err != nil {
-								log.Error("Converting object", zap.Error(err))
-							}
-							sink <- object
-							//fmt.Printf("NewWatcher: %+v\n", object)
-						}
-					case <-close:
-						done = true
-						break W
-					}
-				}
-			}
-			time.Sleep(2 * time.Second)
 		}
 	}()
 	return func() {
@@ -351,25 +234,24 @@ func main() {
 	}
 
 	holder := NewMetrics()
-	extracor := NewMetricsExtracor(holder)
 
-	podConnector, err := NewPodWatcher(config, *namespace)
+	podConnector, err := NewPodLister(config, *namespace)
 	if err != nil {
 		logger.Error("Creating pod connector", zap.Error(err))
 		os.Exit(1)
 	}
-	NewWatcher(podConnector, extracor.Channel(), logger)
+	ShovelList(podConnector, "spec", holder, logger)
 
 	metricsConnector, err := NewMetricsLister(config, *namespace)
 	if err != nil {
 		logger.Error("Creating metrics connector", zap.Error(err))
 		os.Exit(1)
 	}
-	ShovelList(metricsConnector, extracor.Channel(), logger)
+	ShovelList(metricsConnector, "usage", holder, logger)
 
 	memcached, err := memory.NewAdapter(
 		memory.AdapterWithAlgorithm(memory.LRU),
-		memory.AdapterWithCapacity(10000000),
+		memory.AdapterWithCapacity(100),
 	)
 	if err != nil {
 		logger.Error("Creating memory-backed cache", zap.Error(err))
@@ -396,48 +278,9 @@ func main() {
 	}
 }
 
-// Watcher connects to the watch API and converts its result objects
-type Watcher interface {
-	Watch(opts metav1.ListOptions) (watch.Interface, error)
-	Convert(event *watch.Event) (interface{}, error)
-}
-
-// PodWatcher is a Watcher for Pods
-type PodWatcher struct {
-	clientset *kubernetes.Clientset
-	namespace string
-}
-
-// NewPodWatcher constructs a PodWatcher
-func NewPodWatcher(config *rest.Config, namespace string) (*PodWatcher, error) {
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, err
-	}
-	return &PodWatcher{
-		clientset: clientset,
-		namespace: namespace,
-	}, nil
-}
-
-// Watch implementation
-func (p *PodWatcher) Watch(opts metav1.ListOptions) (watch.Interface, error) {
-	return p.clientset.CoreV1().Pods(p.namespace).Watch(opts)
-}
-
-// Convert implementation
-func (p *PodWatcher) Convert(event *watch.Event) (interface{}, error) {
-	pod := apiv1.Pod{}
-	err := scheme.Scheme.Convert(event.Object, &pod, nil)
-	if err != nil {
-		return nil, err
-	}
-	return pod, nil
-}
-
 // Lister reads objects through the get API
 type Lister interface {
-	List(opts metav1.ListOptions) ([]interface{}, error)
+	List(opts metav1.ListOptions) (map[string]*Reading, error)
 }
 
 // MetricsLister is a Lister for Metrics
@@ -459,8 +302,35 @@ func NewMetricsLister(config *rest.Config, namespace string) (*MetricsLister, er
 }
 
 // List returns the result of a listing API call
-func (l *MetricsLister) List(opts metav1.ListOptions) ([]interface{}, error) {
+func (l *MetricsLister) List(opts metav1.ListOptions) (map[string]*Reading, error) {
 	list, err := l.clientset.MetricsV1beta1().PodMetricses(l.namespace).List(opts)
+	if err != nil {
+		return nil, err
+	}
+	return extractPodMetrics(list.Items), nil
+}
+
+// PodLister is a Lister for Metrics
+type PodLister struct {
+	clientset *kubernetes.Clientset
+	namespace string
+}
+
+// NewPodLister constructs a PodWatcher
+func NewPodLister(config *rest.Config, namespace string) (*PodLister, error) {
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return &PodLister{
+		clientset: clientset,
+		namespace: namespace,
+	}, nil
+}
+
+// List returns the result of a listing API call
+func (l *PodLister) List(opts metav1.ListOptions) (map[string]*Reading, error) {
+	list, err := l.clientset.CoreV1().Pods(l.namespace).List(opts)
 	if err != nil {
 		return nil, err
 	}
@@ -468,14 +338,7 @@ func (l *MetricsLister) List(opts metav1.ListOptions) ([]interface{}, error) {
 	for k, v := range list.Items {
 		result[k] = v
 	}
-	return result, nil
-}
-
-func usage(pod apiv1.Pod, labels map[string]string) []string {
-	result := []string{}
-	result = append(result, fmt.Sprintf("ASD{%s}=%d\n", labels, 1))
-
-	return result
+	return extractPodSpecs(list.Items), nil
 }
 
 func renderLabels(labels ...map[string]string) string {
--- go.mod
+++ go.mod
@@ -3,75 +3,34 @@ module github.com/vbence/podstats
 go 1.14
 
 require (
-	cloud.google.com/go v0.38.0 // indirect
-	dmitri.shuralyov.com/gpu/mtl v0.0.0-20191203043605-d42048ed14fd // indirect
-	github.com/Azure/go-autorest/autorest v0.9.0 // indirect
-	github.com/BurntSushi/xgb v0.0.0-20200324125942-20f126ea2843 // indirect
-	github.com/ajstarks/svgo v0.0.0-20200320125537-f189e35d30ca // indirect
-	github.com/bazelbuild/bazel-gazelle v0.21.1 // indirect
-	github.com/davecgh/go-spew v1.1.1
-	github.com/docker/spdystream v0.0.0-20160310174837-449fdfce4d96 // indirect
-	github.com/elazarl/goproxy v0.0.0-20180725130230-947c36da3153 // indirect
-	github.com/evanphx/json-patch v4.2.0+incompatible // indirect
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200420212212-258d9bec320e // indirect
-	github.com/gogo/protobuf v1.3.1
-	github.com/golang/groupcache v0.0.0-20160516000752-02826c3e7903 // indirect
-	github.com/golang/protobuf v1.4.2
-	github.com/google/btree v1.0.0 // indirect
-	github.com/google/gofuzz v1.1.0
-	github.com/google/uuid v1.1.1 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
 	github.com/googleapis/gnostic v0.4.0 // indirect
-	github.com/gophercloud/gophercloud v0.1.0 // indirect
-	github.com/gregjones/httpcache v0.0.0-20180305231024-9cad4c3443a7 // indirect
-	github.com/hashicorp/golang-lru v0.5.1 // indirect
-	github.com/imdario/mergo v0.3.9
-	github.com/json-iterator/go v1.1.9
-	github.com/jung-kurt/gofpdf v1.16.2 // indirect
-	github.com/kr/pretty v0.2.0 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd
-	github.com/modern-go/reflect2 v1.0.1
-	github.com/mxk/go-flowrate v0.0.0-20140419014527-cca7078d478f // indirect
-	github.com/onsi/ginkgo v1.11.0 // indirect
-	github.com/onsi/gomega v1.7.0 // indirect
-	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
-	github.com/phpdave11/gofpdi v1.0.12 // indirect
+	github.com/imdario/mergo v0.3.9 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.6.0 // indirect
 	github.com/victorspringer/http-cache v0.0.0-20190721184638-fe78e97af707
-	github.com/yuin/goldmark v1.1.31 // indirect
-	go.uber.org/atomic v1.6.0
-	go.uber.org/multierr v1.5.0
 	go.uber.org/zap v1.15.0
-	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37
-	golang.org/x/exp v0.0.0-20200513190911-00229845015e // indirect
-	golang.org/x/image v0.0.0-20200430140353-33d19683fad8 // indirect
-	golang.org/x/mobile v0.0.0-20200329125638-4c31acba0007 // indirect
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37 // indirect
 	golang.org/x/mod v0.3.0 // indirect
-	golang.org/x/net v0.0.0-20200528225125-3c3fba18258b
-	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
-	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
-	golang.org/x/sys v0.0.0-20200523222454-059865788121
-	golang.org/x/text v0.3.2
-	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1
+	golang.org/x/net v0.0.0-20200528225125-3c3fba18258b // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/sys v0.0.0-20200523222454-059865788121 // indirect
+	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1 // indirect
 	golang.org/x/tools v0.0.0-20200530233709-52effbd89c51 // indirect
-	gonum.org/v1/netlib v0.0.0-20200317120129-c5a04cffd98a // indirect
-	gonum.org/v1/plot v0.7.0 // indirect
-	google.golang.org/appengine v1.6.6
+	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.24.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/inf.v0 v0.9.1
-	gopkg.in/yaml.v2 v2.3.0
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200506231410-2ff61e1afc86 // indirect
 	k8s.io/api v0.16.10
 	k8s.io/apimachinery v0.16.10
 	k8s.io/client-go v0.16.10
-	k8s.io/code-generator v0.16.10 // indirect
-	k8s.io/klog v1.0.0
 	k8s.io/metrics v0.16.10
-	k8s.io/utils v0.0.0-20200529193333-24a76e807f40
-	modernc.org/mathutil v1.1.1 // indirect
-	sigs.k8s.io/structured-merge-diff v0.0.0-20190525122527-15d366b2352e // indirect
-	sigs.k8s.io/structured-merge-diff/v3 v3.0.0 // indirect
-	sigs.k8s.io/yaml v1.2.0
+	k8s.io/utils v0.0.0-20200529193333-24a76e807f40 // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
 )
